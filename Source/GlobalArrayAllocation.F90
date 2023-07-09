59bb1c91571b29d8b6d152bfa65855a179def946
Toshiyuki <tbandai@lbl.gov>
2023-07-09 11:05:42 -0700

    fixed notation and comments

--- Source/GlobalArrayAllocation.F90
+++ Source/GlobalArrayAllocation.F90
@@ -269,16 +269,16 @@ IF (ny == 1 .AND. nz == 1) THEN
 
   IF (ALLOCATED(mu_water)) THEN
     DEALLOCATE(mu_water)
-    ALLOCATE(mu_water(nx,ny,nz))
+    ALLOCATE(mu_water(nx+1,ny,nz))
   ELSE
-    ALLOCATE(mu_water(nx,ny,nz))
+    ALLOCATE(mu_water(nx+1,ny,nz))
   END IF
 
-  IF (ALLOCATED(rho_water2)) THEN
-    DEALLOCATE(rho_water2)
-    ALLOCATE(rho_water2(nx,ny,nz))
+  IF (ALLOCATED(rho_water_2)) THEN
+    DEALLOCATE(rho_water_2)
+    ALLOCATE(rho_water_2(nx,ny,nz))
   ELSE
-    ALLOCATE(rho_water2(nx,ny,nz))
+    ALLOCATE(rho_water_2(nx,ny,nz))
   END IF
 
 !!!  IF (ALLOCATED(por)) THEN
@@ -362,11 +362,11 @@ ELSE IF (nx > 1 .AND. ny > 1 .AND. nz == 1) THEN
   ELSE
     ALLOCATE(mu_water(nx,ny,nz))
   END IF
-  IF (ALLOCATED(rho_water2)) THEN
-    DEALLOCATE(rho_water2)
-    ALLOCATE(rho_water2(nx,ny,nz))
+  IF (ALLOCATED(rho_water_2)) THEN
+    DEALLOCATE(rho_water_2)
+    ALLOCATE(rho_water_2(nx,ny,nz))
   ELSE
-    ALLOCATE(rho_water2(nx,ny,nz))
+    ALLOCATE(rho_water_2(nx,ny,nz))
   END IF
 !!!  IF (ALLOCATED(por)) THEN
 !!!    DEALLOCATE(por)
